8e8d62481d3da92d40bf459011badaf767682b87
JoseCM <josemartins90@gmail.com>
2018-12-31 18:25:44 +0000

    Add FreeRTOS to zone 1
    - runing a single task (rainbow led fade)

---
+++ zone1/Source/portable/GCC/RISCV/port.c
@@ -0,0 +1,196 @@
+/**
+ * Copyright(C) 2018 Hex Five Security, Inc. - All Rights Reserved
+ *
+ * @author 	Sandro Pinto <sandro2pinto@gmail.com>
+ * @author 	José Martins <josemartins90@gmail.com>
+ *
+ * TODO: add credits to original sifive port
+ * 
+ */
+
+/* Scheduler includes. */
+#include "FreeRTOS.h"
+#include "task.h"
+#include "portmacro.h"
+
+/*SiFive Includes */
+#include "platform.h"
+
+/*MultiZone Includes */
+#include "libhexfive.h"
+
+/* Standard Includes */
+#include <stdlib.h>
+#include <unistd.h>
+
+
+/* Each task maintains its own interrupt status in the critical nesting
+variable. */
+UBaseType_t uxCriticalNesting = 0xaaaaaaaa;
+
+/* Set to 1 to pend a context switch from an ISR. */
+volatile UBaseType_t ulPortYieldRequired = pdFALSE;
+
+/* Counts the interrupt nesting depth.  A context switch is only performed if
+if the nesting depth is 0. */
+volatile UBaseType_t ulPortInterruptNesting = 0UL;
+
+/*
+ * Used to catch tasks that attempt to return from their implementing function.
+ */
+static void prvTaskExitError( void );
+
+/*-----------------------------------------------------------*/
+
+void vPortEnterCritical( void )
+{
+
+	portDISABLE_INTERRUPTS();
+
+	uxCriticalNesting++;
+}
+/*-----------------------------------------------------------*/
+
+void vPortExitCritical( void )
+{
+	configASSERT( uxCriticalNesting );
+	uxCriticalNesting--;
+	if( uxCriticalNesting == 0 )
+	{
+		portENABLE_INTERRUPTS();
+	}
+
+	return;
+}
+
+/*-----------------------------------------------------------*/
+
+/* Clear current interrupt mask and set given mask */
+void vPortClearInterruptMask(int mask)
+{
+	/**
+	 * MULTIZONE: the nanokernel does not allow a zone to directly write
+	 * machine registers.
+	 * TODO: Implement this using the multizone exception register/deregister
+	 * API.
+	 */
+	//	write_csr(mie,mask);
+}
+/*-----------------------------------------------------------*/
+
+/* Set interrupt mask and return current interrupt enable register */
+int xPortSetInterruptMask()
+{
+	//uint32_t ret;
+	/**
+	 * MULTIZONE: the nanokernel does not allow a zone to directly write
+	 * machine registers.
+	 * TODO: Implement this using the multizone exception register/deregister
+	 * API.
+	 */
+	//ret = read_csr(mie);
+	//write_csr(mie,0);
+	//return ret;
+	return 0;
+}
+
+/*-----------------------------------------------------------*/
+
+StackType_t *pxPortInitialiseStack( StackType_t *pxTopOfStack, TaskFunction_t pxCode, void *pvParameters )
+{
+	register int gp asm("gp");
+	/**
+	 * Simulate the stack frame as it would be created by an interrupt. 
+	 * This includes the initial multizone stack frame with the mepc and mcause at the bottom.
+	 * 
+	 * TODO: initialize all stack registers with a default value.
+	 */
+	pxTopOfStack--;
+	*pxTopOfStack = (StackType_t)pxCode;	 /* Start address @ mepc MultiZone*/
+	pxTopOfStack--;
+	*pxTopOfStack = 0;	/* Set initial mcause to innocous value of non-existing interrupt (user software) */
+	pxTopOfStack -= 22;
+	*pxTopOfStack = (StackType_t)pvParameters;	/* Register a0 */
+	pxTopOfStack -=7;
+	*pxTopOfStack = (StackType_t)gp;
+	pxTopOfStack -=2;
+	*pxTopOfStack = (StackType_t)prvTaskExitError; /* Register ra */
+	pxTopOfStack--;
+
+	return pxTopOfStack;
+}
+/*-----------------------------------------------------------*/
+
+void prvTaskExitError( void )
+{
+	/* A function that implements a task must not exit or attempt to return to
+	its caller as there is nothing to return to.  If a task wants to exit it
+	should instead call vTaskDelete( NULL ).
+	Artificially force an assert() to be triggered if configASSERT() is
+	defined, then stop here so application writers can catch the error. */
+	configASSERT( uxCriticalNesting == ~0UL );
+	portDISABLE_INTERRUPTS();
+	for( ;; );
+}
+/*-----------------------------------------------------------*/
+
+/*Entry Point for Machine Timer Interrupt Handler*/
+void vPortSysTickHandler(){
+	
+	/* Calculate next compare value */
+	const uint64_t now = ECALL_CSRR_MTIME();
+	const uint64_t then = now + (configRTC_CLOCK_HZ / configTICK_RATE_HZ);
+
+	/* Increment the RTOS tick. */
+	if( xTaskIncrementTick() != pdFALSE )
+	{
+		ulPortYieldRequired = pdTRUE;
+	}
+
+	/* Request next timer interrupt */
+	ECALL_CSRW_MTIMECMP(then);
+}
+/*-----------------------------------------------------------*/
+
+extern void _timer_handler();
+extern void _syncexception_entry();
+extern void _interrupt_handler();
+
+void vConfigureTickInterrupt()	{
+
+	// Setup mtimer handler
+	ECALL_TRP_VECT(0x3, _timer_handler);
+
+	/* Calculate first tick timer compare */
+	const uint64_t now = ECALL_CSRR_MTIME();
+	const uint64_t then = now + (configRTC_CLOCK_HZ / configTICK_RATE_HZ);
+
+	/* Request first tick interrupt */
+    ECALL_CSRW_MTIMECMP(then);
+}
+/*-----------------------------------------------------------*/
+
+
+void vPortSetup(){
+
+	/**
+	 * MULTIZONE: all available synchronous exception handlers are registered 
+	 * to the same initial handler, which will decode the mcause that arrives 
+	 * in a message from the nanokernel and redirect to the final handler.
+	 * 
+	 * TODO: if more exceptions become available in the API they should be 
+	 * added here.
+	 */
+
+	ECALL_TRP_VECT(0x0, _syncexception_entry); // 0x0 Instruction address misaligned
+	ECALL_TRP_VECT(0x1, _syncexception_entry); // 0x1 Instruction access fault
+	ECALL_TRP_VECT(0x2, _syncexception_entry); // 0x2 Illegal Instruction
+    ECALL_TRP_VECT(0x4, _syncexception_entry); // 0x4 Load address misaligned
+    ECALL_TRP_VECT(0x5, _syncexception_entry); // 0x5 Load access fault
+    ECALL_TRP_VECT(0x6, _syncexception_entry); // 0x6 Store/AMO address misaligned
+	ECALL_TRP_VECT(0x7, _syncexception_entry); // 0x7 Store access fault
+
+	vConfigureTickInterrupt();
+	uxCriticalNesting = 0;
+}
+/*-----------------------------------------------------------*/
--- zone1/main.c
+++ zone1/main.c
@@ -1,23 +1,352 @@
-/* Copyright(C) 2018 Hex Five Security, Inc. - All Rights Reserved */
+/**
+ * Copyright(C) 2018 Hex Five Security, Inc. - All Rights Reserved
+ *
+ * @author 	Sandro Pinto <sandro2pinto@gmail.com>
+ * @author 	José Martins <josemartins90@gmail.com>
+ *
+ * TODO: add credits to original sifive port
+ * 
+ */
 
-#include <platform.h>
+/* Kernel includes. */
+#include "FreeRTOS.h" /* Must come first. */
+#include "task.h"     /* RTOS task related API prototypes. */
+#include "queue.h"    /* RTOS queue related API prototypes. */
+#include "timers.h"   /* Software timer related API prototypes. */
+#include "semphr.h"   /* Semaphore related API prototypes. */
+
+/* TODO Add any manufacture supplied header files can be included
+here. */
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include "platform.h"
+#include "plic_driver.h"
+
 #include <libhexfive.h>
 
-int main (void){
 
-	//volatile int w=0; while(1){w++;}
-	//while(1) ECALL_YIELD();
 
-	while(1){
+/*-----------------------------------------------------------*/
 
-		int msg[4]={0,0,0,0};
+extern void _interrupt_entry();
+static void prvSetupHardware( void );
+static void prvTask( void *pvParameters );
 
-		ECALL_RECV(4, msg);
+/*-----------------------------------------------------------*/
 
-		if (msg[0]) ECALL_SEND(4, msg);
+// Structures for registering different interrupt handlers
+// for different parts of the application.
+typedef void (*function_ptr_t) (void);
+void no_interrupt_handler (void) {};
+function_ptr_t g_ext_interrupt_handlers[PLIC_NUM_INTERRUPTS];
+function_ptr_t localISR[32]; 
 
-		ECALL_YIELD();
+// Instance data for the PLIC.
+plic_instance_t g_plic;
 
-	}
+/*-----------------------------------------------------------*/
 
-} // main
+#define LOCAL_INT_BTN_0  0
+#define LOCAL_INT_BTN_1  1
+#define LOCAL_INT_BTN_2  2
+
+#define RED_LED_OFFSET   0
+#define GREEN_LED_OFFSET 1
+#define BLUE_LED_OFFSET  2
+
+#define BUTTON_0_OFFSET 15
+#define BUTTON_1_OFFSET 30
+#define BUTTON_2_OFFSET 31
+
+#define GPIO_INT_BASE 7
+#define INT_DEVICE_BUTTON_0 (GPIO_INT_BASE + BTN0)
+#define INT_DEVICE_BUTTON_1 (GPIO_INT_BASE + BTN1)
+#define INT_DEVICE_BUTTON_2 (GPIO_INT_BASE + BTN2)
+
+#define LD1_RED_ON PWM_REG(PWM_CMP1)  = 0x00;
+#define LD1_GRN_ON PWM_REG(PWM_CMP2)  = 0x00;
+#define LD1_BLU_ON PWM_REG(PWM_CMP3)  = 0x00;
+
+#define LD1_RED_OFF PWM_REG(PWM_CMP1)  = 0xFF;
+#define LD1_GRN_OFF PWM_REG(PWM_CMP2)  = 0xFF;
+#define LD1_BLU_OFF PWM_REG(PWM_CMP3)  = 0xFF;
+
+#define IRQ_M_LOCAL        16
+#define MCAUSE_CAUSE       0x3FFUL
+#define IRQ_M_EXT          11
+
+// Application specific.
+static volatile int led = GREEN_LED_OFFSET;
+
+/*-------------------------------------------------------------*/
+
+int main(void)
+{
+
+    // while(1){
+	// 	int msg[4]={0,0,0,0};
+	// 	ECALL_RECV(4, msg);
+	// 	if (msg[0]) ECALL_SEND(4, msg);
+	// 	ECALL_YIELD();
+	// }
+
+
+    /* Configure the system ready to run the demo.  The clock configuration
+    can be done here if it was not done before main() was called. */
+    prvSetupHardware();
+
+    /* Create the task. */
+	xTaskCreate(    prvTask,
+					"prvTask",
+					configMINIMAL_STACK_SIZE,
+					NULL,
+					0x02,
+					NULL );
+
+
+    /* Start the tasks and timer running. */
+    vTaskStartScheduler();
+
+    /* If all is well, the scheduler will now be running, and the following line
+    will never be reached.  If the following line does execute, then there was
+    insufficient FreeRTOS heap memory available for the idle and/or timer tasks
+    to be created.  See the memory management section on the FreeRTOS web site
+    for more details.  */
+    for( ;; );
+}
+/*-----------------------------------------------------------*/
+
+
+static void prvTask( void *pvParameters )
+{
+
+    uint16_t r=0x3F;
+    uint16_t g=0;
+    uint16_t b=0;
+
+    PWM_REG(PWM_CFG)   = 0;
+    PWM_REG(PWM_CFG)   = (PWM_CFG_ENALWAYS) | (PWM_CFG_ZEROCMP) | (PWM_CFG_DEGLITCH);
+    PWM_REG(PWM_COUNT) = 0;
+
+    // The LEDs are intentionally left somewhat dim.
+    PWM_REG(PWM_CMP0)  = 0xFE;
+
+    while(1){
+
+        // const uint64_t T1 = ECALL_CSRR_MTIME() + 400; //12*RTC_FREQ/1000;
+        // while (ECALL_CSRR_MTIME() < T1) ECALL_YIELD();
+        vTaskDelay(40/portTICK_PERIOD_MS);
+
+        if(r > 0 && b == 0){ r--; g++; }
+        if(g > 0 && r == 0){ g--; b++; }
+        if(b > 0 && g == 0){ r++; b--; }
+
+        PWM_REG(PWM_CMP1)  = 0xFF - (r >> 2);
+        PWM_REG(PWM_CMP2)  = 0xFF - (g >> 2);
+        PWM_REG(PWM_CMP3)  = 0xFF - (b >> 2);
+
+    }// While (1)
+}
+
+/*ISR triggered by Button 3 */
+
+void button_1_handler(void){ // local interrupt
+
+	// GPIO_REG(GPIO_OUTPUT_VAL) &= ~(0x1 << led); // LED OFF
+
+	// led = (led==GREEN_LED_OFFSET ? BLUE_LED_OFFSET :
+	// 	   led==BLUE_LED_OFFSET  ? RED_LED_OFFSET :
+	// 			   	   	   	   	   GREEN_LED_OFFSET
+	// );
+
+    ECALL_SEND(1, ((int[]){'p',0,0,0}));
+
+	// volatile uint64_t * now, then;
+	// now = (volatile uint64_t*)(CLINT_CTRL_ADDR + CLINT_MTIME);
+	// then = *now + 500*32768/1000;
+	// while (*now < then) ECALL_YIELD();
+
+	//GPIO_REG(GPIO_RISE_IP) |= (1<<BUTTON_3_OFFSET);
+
+}
+
+/*-----------------------------------------------------------*/
+
+void handle_interrupt(unsigned long mcause){
+
+  /* check if global*/
+  if(!((mcause & MCAUSE_CAUSE) == IRQ_M_EXT))   {
+    localISR[mcause & MCAUSE_CAUSE]();
+  } else {
+      //global interrupt
+    plic_source int_num  = PLIC_claim_interrupt(&g_plic);
+    g_ext_interrupt_handlers[int_num]();
+    PLIC_complete_interrupt(&g_plic, int_num);
+  }
+
+}
+
+/*-----------------------------------------------------------*/
+
+unsigned long handle_syncexception(unsigned long mcause, unsigned long mtval, unsigned long mepc){
+
+    switch(mcause){
+
+        case 0x0: // Instruction address misaligned 
+            break;
+        case 0x1: // Instruction access fault       
+            break;
+        case 0x2: // Illegal Instruction            
+            break;
+        case 0x4: // Load address misaligned        
+            break; 
+        case 0x5: // Load access fault              
+            break;
+        case 0x6: // Store/AMO address misaligned   
+            break;
+        case 0x7: // Store access fault             
+            break;
+        default:
+            break;
+    }
+
+    /**
+     *  MULTIZONE: this method must rearrange the epc, and set it to where it 
+     *  wants to return.
+     */
+    mepc += 4;
+    return mepc;
+}
+
+/*-----------------------------------------------------------*/
+
+
+//enables interrupt and assigns handler
+void enable_interrupt(uint32_t int_num, uint32_t int_priority, function_ptr_t handler) {
+    g_ext_interrupt_handlers[int_num] = handler;
+    PLIC_set_priority(&g_plic, int_num, int_priority);
+    PLIC_enable_interrupt (&g_plic, int_num);
+    /**
+     * MULTIZONE: every interrupt must be registered to the same initial handler. 
+     * After decoding the mcause and aknowleding the interrupt on the PLIC (in case
+     * of a global interrupt) execution will be redirected to the final handler.
+     */
+    ECALL_IRQ_VECT(int_num, _interrupt_entry);
+}
+/*-----------------------------------------------------------*/
+
+/*
+ *enables the plic and programs handlers
+**/
+void interrupts_init(  ) {
+    
+    /**
+     * MULTIZONE: multizone API does not allow us to enable or disable
+     * interrupts. After we register an handler for a interrupt it is 
+     * permantently enabled. At this point, no handler was registered,
+     * so we know all interrupts are disabled.
+     */
+    // Disable the machine & timer interrupts until setup is done.
+    //clear_csr(mie, MIP_MEIP);
+    //clear_csr(mie, MIP_MTIP);
+
+
+  //setup PLIC
+  PLIC_init(&g_plic,
+	    PLIC_BASE,
+	    PLIC_NUM_INTERRUPTS,
+	    PLIC_NUM_PRIORITIES);
+
+  //assign interrupts to default handler
+  for (int ii = 0; ii < PLIC_NUM_INTERRUPTS; ii ++){
+    g_ext_interrupt_handlers[ii] = no_interrupt_handler;
+  }
+
+  for (int isr = 0; isr < 32; isr++){
+    localISR[isr] = no_interrupt_handler;
+  }
+
+    /**
+     * MULTIZONE: multizone API has no notion of a global exertnal interrupt
+     * enable. As mentioned in the comment above, after an handler for an
+     * interrupt is registered, that interrupt is enabled (permantely).
+     */
+    // Enable the Machine-External bit in MIE
+    //set_csr(mie, MIP_MEIP);
+
+}
+/*-----------------------------------------------------------*/
+
+
+/*configures Button3 as a local interrupt*/
+void b1_irq_init() {
+
+    //dissable hw io function
+    GPIO_REG(GPIO_IOF_EN ) &=  ~(1 << BUTTON_1_OFFSET);
+
+    //set to input
+    GPIO_REG(GPIO_INPUT_EN)   |= (1<<BUTTON_1_OFFSET);
+    GPIO_REG(GPIO_PULLUP_EN)  |= (1<<BUTTON_1_OFFSET);
+
+    //set to interrupt on rising edge
+    GPIO_REG(GPIO_RISE_IE)    |= (1<<BUTTON_1_OFFSET);
+
+    //enable the interrupt
+    ECALL_IRQ_VECT(16+LOCAL_INT_BTN_0, _interrupt_entry);
+    ECALL_IRQ_VECT(16+LOCAL_INT_BTN_1, _interrupt_entry);
+    ECALL_IRQ_VECT(16+LOCAL_INT_BTN_2, _interrupt_entry);
+    localISR[IRQ_M_LOCAL + LOCAL_INT_BTN_0] = button_1_handler;
+    localISR[IRQ_M_LOCAL + LOCAL_INT_BTN_1] = button_1_handler;
+    localISR[IRQ_M_LOCAL + LOCAL_INT_BTN_2] = button_1_handler;
+    //enable_interrupt(INT_DEVICE_BUTTON_3, 2, &b0_ISR);
+}
+/*-----------------------------------------------------------*/
+
+static void prvSetupHardware( void )
+{
+
+    interrupts_init();
+    b1_irq_init();
+}
+
+
+/*--- Definition of hooks useful for debugging purposes ---*/
+
+void vApplicationMallocFailedHook( void )
+{
+    /* The malloc failed hook is enabled by setting
+    configUSE_MALLOC_FAILED_HOOK to 1 in FreeRTOSConfig.h.
+
+    Called if a call to pvPortMalloc() fails because there is insufficient
+    free memory available in the FreeRTOS heap.  pvPortMalloc() is called
+    internally by FreeRTOS API functions that create tasks, queues, software
+    timers, and semaphores.  The size of the FreeRTOS heap is set by the
+    configTOTAL_HEAP_SIZE configuration constant in FreeRTOSConfig.h. */
+    write(1,"malloc failed\n", 14);
+    
+    for( ;; );
+}
+/*-----------------------------------------------------------*/
+
+void vApplicationStackOverflowHook( TaskHandle_t xTask, signed char *pcTaskName )
+{
+    ( void ) pcTaskName;
+    ( void ) xTask;
+
+    /* Run time stack overflow checking is performed if
+    configconfigCHECK_FOR_STACK_OVERFLOW is defined to 1 or 2.  This hook
+    function is called if a stack overflow is detected.  pxCurrentTCB can be
+    inspected in the debugger if the task name passed into this function is
+    corrupt. */
+    write(1, "Stack Overflow\n", 15);
+    for( ;; );
+}
+/*-----------------------------------------------------------*/
+
+void vApplicationIdleHook( void )
+{
+    ECALL_YIELD();
+}
+
+
