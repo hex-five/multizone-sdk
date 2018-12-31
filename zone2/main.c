bf270cc2abeed6ebcdbe629fb4a8181914568a1b
Boran Car <boran.car@gmail.com>
2018-12-31 17:38:45 +0000

    Add ping test program

--- zone2/main.c
+++ zone2/main.c
@@ -1,27 +1,126 @@
-/* Copyright(C) 2018 Hex Five Security, Inc. - All Rights Reserved */
+/*
+ * Copyright(C) 2018 Hex Five Security, Inc. - Some rights reserved.
+ *
+ * This file is part of multizone-sdk/zone2.
+ *
+ * multizone-sdk/zone2 is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 2 of the License, or
+ * (at your option) any later version.
+ *
+ * multizone-sdk/zone2 is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with multizone-sdk/zone2. If not, see <https://www.gnu.org/licenses/>.
+ */
 
 #include <platform.h>
 #include <libhexfive.h>
 
-int main (void){
+#define RV32
 
-	//volatile int w=0; while(1){w++;}
-	//while(1) ECALL_YIELD();
+#include <pico_stack.h>
+#include <pico_ipv4.h>
+#include <pico_icmp4.h>
+#include <pico_dev_xemaclite.h>
 
-	while(1){
+#define NUM_PING 10
 
-		int msg[4]={0,0,0,0};
+static int finished = 0;
 
-		ECALL_RECV(4, msg);
+void *pico_zalloc(size_t size)
+{
+    void *ptr = malloc(size);
+    if(ptr)
+        memset(ptr, 0u, size);
 
-		if (msg[0]) ECALL_SEND(4, msg);
+    return ptr;
+}
 
-		ECALL_YIELD();
+void pico_free(void *ptr)
+{
+    free(ptr);
+}
 
-	}
+pico_time PICO_TIME_MS(void)
+{
+    return (pico_time)(ECALL_CSRR_MCYCLE() * 1000 / CPU_FREQ);
+}
 
-} // main
+pico_time PICO_TIME(void)
+{
+    return (pico_time)(ECALL_CSRR_MCYCLE() / CPU_FREQ);
+}
 
+void PICO_IDLE(void)
+{
+    ECALL_YIELD();
+}
 
+/* gets called when the ping receives a reply, or encounters a problem */
+void cb_ping(struct pico_icmp4_stats *s)
+{
+    char host[30];
+    pico_ipv4_to_string(host, s->dst.addr);
+    if (s->err == 0) {
+        /* if all is well, print some pretty info */
+        printf("%lu bytes from %s: icmp_req=%lu ttl=%lu time=%lu ms\n", s->size,
+                host, s->seq, s->ttl, (long unsigned int)s->time);
+    } else {
+        /* if something went wrong, print it and signal we want to stop */
+        printf("PING %lu to %s: Error %d\n", s->seq, host, s->err);
+    }
+}
 
+int main(int argc, char *argv[]){
+    int id;
+    struct pico_ip4 ipaddr, netmask;
+    struct pico_device* dev;
 
+    uint16_t bmsr = 0;
+
+    while ((bmsr & 0x4) == 0) {
+        bmsr = pico_xemaclite_mdio_read(0x01, 0x01);
+    }
+
+    /* initialise the stack. Super important if you don't want ugly stuff like
+     * segfaults and such! */
+    pico_stack_init();
+
+    printf("pico stack initialized\n");
+
+    dev = pico_xemaclite_create();
+    if (!dev) {
+        printf("Could not initialize device\n");
+        return -1;
+    }
+
+    /* assign the IP address to the tap interface */
+    pico_string_to_ipv4("192.168.1.2", &ipaddr.addr);
+    pico_string_to_ipv4("255.255.255.0", &netmask.addr);
+    pico_ipv4_link_add(dev, ipaddr, netmask);
+
+    printf("starting ping\n");
+    id = pico_icmp4_ping("192.168.1.1", NUM_PING, 1000, 10000, 64, cb_ping);
+
+    if (id == -1) {
+        printf("Could not ping\n");
+        return -1;
+    }
+
+    /* keep running stack ticks to have picoTCP do its network magic. Note that
+     * you can do other stuff here as well, or sleep a little. This will impact
+     * your network performance, but everything should keep working (provided
+     * you don't go overboard with the delays). */
+    while (finished != 1)
+    {
+        pico_stack_tick();
+        ECALL_YIELD();
+    }
+
+    printf("finished !\n");
+    return 0;
+}
