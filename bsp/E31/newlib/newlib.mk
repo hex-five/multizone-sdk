6ae2f85a72349d8eb4a3ddd542b243a5bb9a86fa
cesare <cesare@hex-five.com>
2019-01-31 17:01:34 -0800

    .

--- bsp/E31/newlib/newlib.mk
+++ bsp/E31/newlib/newlib.mk
@@ -32,12 +32,14 @@ CFLAGS += -msmall-data-limit=8
 CFLAGS += -mdiv
 
 HEX = $(subst .elf,.hex,$(TARGET))
-RISCV_OBJCOPY := $(dir $(CC))$(subst -gcc,-objcopy,$(notdir $(CC)))
-CLEAN_OBJS += $(HEX) 
+LST = $(subst .elf,.lst,$(TARGET))
+CLEAN_OBJS += $(HEX)
+CLEAN_OBJS += $(LST) 
 
 $(TARGET): $(LINK_OBJS) $(LINK_DEPS)
 	$(CC) $(CFLAGS) $(INCLUDES) $(LINK_OBJS) -o $@ $(LDFLAGS)
-	$(RISCV_OBJCOPY) -O ihex $(TARGET) $(HEX) --gap-fill 0x00
+	$(OBJCOPY) -O ihex $(TARGET) $(HEX) --gap-fill 0x00
+	$(OBJDUMP) --all-headers --demangle --disassemble --file-headers --wide -D $(TARGET) > $(LST)
 
 $(ASM_OBJS): %.o: %.S $(HEADERS)
 	$(CC) $(CFLAGS) $(INCLUDES) -c -o $@ $<
